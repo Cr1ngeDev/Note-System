9a5d309ba824dd3b62c5a2915a4453ca43f1087d
Ihor <forstudy757@gmail.com>
2023-12-27 23:40:46 +0200

    Test finish

--- index.php
+++ index.php
@@ -6,4 +6,5 @@ require 'app/helpers.php';
 require 'app/Kernel/Database/database.php';
 
 require 'app/Router.php';
+
 //docker exec -i mysql mysql -uihor -pIhorbasegi75_ notes < ./database/dump.sql
