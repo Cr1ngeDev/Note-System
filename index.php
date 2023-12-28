1a7137e8a7ed17e408313adeb40a84d10fa4a628
Ihor <forstudy757@gmail.com>
2023-12-28 13:30:14 +0200

    a

--- index.php
+++ index.php
@@ -6,5 +6,3 @@ require 'app/helpers.php';
 require 'app/Kernel/Database/database.php';
 
 require 'app/Router.php';
-
-//docker exec -i mysql mysql -uihor -pIhorbasegi75_ notes < ./database/dump.sql
